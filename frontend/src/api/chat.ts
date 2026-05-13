828989e1e023fe934737295ecc6c2f6a913b8134
KEVINWONG <easymoneysniperchris@gmail.com>
2026-05-13 19:49:17 +0800

    Delete frontend/src/api/chat.ts

--- frontend/src/api/chat.ts
+++
@@ -1,6 +0,0 @@
-import { http } from './http';
-
-export async function sendChatMessage(message: string, conversationId?: string) {
-  const { data } = await http.post('/chat', { message, conversationId });
-  return data;
-}
